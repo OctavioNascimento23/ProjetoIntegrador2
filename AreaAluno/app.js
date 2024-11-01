5850e1ed128335b9e6fb22edc2aa58926fab5db6
dusanvido <161546934+dusanvido@users.noreply.github.com>
2024-11-01 18:14:47 -0300

    app.js

---
+++ AreaAluno/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Login from './Login';
+import Register from './Register';
+
+function App() {
+  return (
+    <div>
+      <h1>Sistema de Autenticação</h1>
+      <Login />
+      <Register />
+    </div>
+  );
+}
+
+export default App;
